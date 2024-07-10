7033f77d8066a7fbd44ab476fc09c1057d8899dc
xVicity <xvicityy@gmail.com>
2024-07-10 09:13:08 -0500

    Update a.lua!!

--- a.lua
+++ a.lua
@@ -1072,7 +1072,7 @@ end
 function Library:tab(options)
 	options = self:set_defaults({
 		Name = "New Tab",
-		Icon = "rbxassetid://8569322835"
+		Icon = "rbxassetid://14502433595"
 	}, options)
 
 	local tab = self.container:object("ScrollingFrame", {
